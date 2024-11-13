c5f52376cfd930278d6206194c9485645c281395
Fruscianty <anri.2002.xndr@gmail.com>
2024-11-14 00:08:18 +0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ let a = 5;
 let b = 7;
 console.log(a+b);
 
-console.log(a*b)
+console.log(a*b);
 
 const name = "Anri";
 console.log("Hello" + name + "!");
 
 const stringOne = "string and";
-const stringTwo = " second string"
+const stringTwo = " second string";
 console.log(stringOne + stringTwo);
